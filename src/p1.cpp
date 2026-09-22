92cd11cfb9a89a126f57bfbef56a2201af2a89fe
MikaelNineza <lnineza@ualberta.ca>
2026-09-22 15:37:19 -0600

    completed code for p1

--- src/p1.cpp
+++ src/p1.cpp
@@ -1,1 +1,48 @@
-int main() { return 0; }
+#include <iostream>
+#include <random>
+#include <vector>
+#include "timer.h"
+
+constexpr size_t SIZE = 16000000;
+constexpr size_t DIMENSION_SIZE = 4000;
+
+int main() {
+    std::vector<int> rowMajArr(SIZE);
+    std::vector<int> colMajArr(SIZE);
+    std::mt19937_64 rng(0);
+    uint64_t next;
+    for (size_t i = 0; i < DIMENSION_SIZE; ++i) {
+        for (size_t j = 0; j < DIMENSION_SIZE; ++j) {
+                next = rng();
+                rowMajArr[i*DIMENSION_SIZE + j] = next;
+                colMajArr[i + j*DIMENSION_SIZE] = next;
+        }
+    }
+
+    uint64_t sum1 = 0;
+    uint64_t sum2 = 0;
+    Timer timer;
+    uint64_t rowMajTime = 0;
+    uint64_t colMajTime = 0;
+    timer.restart();
+    for (size_t i = 0; i < DIMENSION_SIZE; ++i) {
+        for (size_t j = 0; j < DIMENSION_SIZE; ++j) {
+                sum1 += rowMajArr[i*DIMENSION_SIZE + j];
+        }
+    }
+    rowMajTime = timer.click<Timer::Micros>();
+
+    timer.restart();
+    for (size_t i = 0; i < DIMENSION_SIZE; ++i) {
+        for (size_t j = 0; j < DIMENSION_SIZE; ++j) {
+                sum2 += colMajArr[i + j*DIMENSION_SIZE];
+        }
+    }
+    colMajTime = timer.click<Timer::Micros>();
+
+    std::cout << rowMajTime << " " << sum1 << "\n";
+    std::cout << colMajTime << " " << sum2 << "\n";
+
+
+    return 0;
+}
--- src/timer.h
+++ src/timer.h
@@ -1,6 +1,40 @@
 #ifndef TIMER_H
 #define TIMER_H
 
-// replace with yours from the prep...
+#include <chrono>
+#include <cstdint>
+
+class Timer {
+    public:
+        using Nanos = std::chrono::nanoseconds;
+        using Micros = std::chrono::microseconds;
+        using Millis = std::chrono::milliseconds;
+        using Seconds = std::chrono::seconds;
+        using Minutes = std::chrono::minutes;
+        using Hours = std::chrono::hours;
+
+        Timer() : t1(std::chrono::steady_clock::now()) {
+        }
+
+        void restart() {
+            t1 = std::chrono::steady_clock::now();
+        }
+
+        template <typename T> 
+        uint64_t click() {
+            auto t2 = std::chrono::steady_clock::now();
+            auto timeElapsed = std::chrono::duration_cast<T>(t2 - t1).count();
+            t1 = t2;
+            return timeElapsed;
+        }
+        template <typename T> 
+        uint64_t glance() const {
+            auto t2 = std::chrono::steady_clock::now();
+            return std::chrono::duration_cast<T>(t2 - t1).count();
+        }
+    
+    private:
+        std::chrono::time_point<std::chrono::steady_clock> t1;
+};
 
 #endif  // TIMER_H
