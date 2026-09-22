567e26427130c994bf2ec6b878cb976f16918232
MikaelNineza <lnineza@ualberta.ca>
2026-09-22 16:20:23 -0600

    completed part 2 a

---
+++ src/p2a.h
@@ -0,0 +1,45 @@
+#ifndef MORTON_H
+#define MORTON_H
+
+#include <cassert>
+#include <cstdint>
+
+inline uint64_t expand(uint64_t input, uint32_t scale) {
+    assert(scale > 0);
+    if (scale == 1) {
+        return input;
+    }
+
+    const uint32_t maxLen = 64;
+    uint64_t newVal = 0;
+
+    for (uint32_t i = 0; i < maxLen; ++i) {
+        uint64_t currBit  = (input >> i) & 1;
+        uint64_t newPos = i * scale;
+
+        if (newPos < maxLen) {
+            currBit <<= newPos;
+            newVal |= currBit;
+        }
+    }
+
+    return newVal;
+}
+
+inline uint64_t morton3d(uint64_t x, uint64_t y, uint64_t z) {
+    uint64_t result = 0;
+    uint64_t x2 = x;
+    uint64_t y2 = expand(y, 2) << 2;
+    uint64_t z2 = expand(z, 3) << 2;
+
+    for (uint64_t i = 0; i < 64; ++i) {
+        result |= (x2 | y2 | z2);
+        x2 >>= 1;
+        y2 >>= 1;
+        z2 >>= 1;
+    }
+
+    return result;
+}
+
+#endif  // MORTON_H
